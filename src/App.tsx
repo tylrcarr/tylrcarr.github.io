55659c506bc0b7f59283eb5e020fd49144551c8b
Tyler Carr <tyler.carr@mimacom.com>
2024-11-23 11:27:44 -0500

    added material ui, updated home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React, {useState} from 'react';
 import './App.css';
+import {Button, Container, createTheme, CssBaseline, ThemeProvider, Typography} from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        mode: "dark",
+    }
+})
 
 function App() {
     const [count, setCount] = useState<number>(0);
     return (
-        <div>
-            <p>{count}</p>
-            <button onClick={() => setCount(prev => prev + 1)}>click</button>
-        </div>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Container>
+                <Typography variant="body1">{count}</Typography>
+                <Button onClick={() => setCount(prev => prev + 1)}>click</Button>
+            </Container>
+        </ThemeProvider>
     );
 }
 
